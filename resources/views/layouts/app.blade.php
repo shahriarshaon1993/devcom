9d8dc76c8c4287e9d7276106a386d83b8bf4cfa7
Shaon Shahriar <shahriarshaon1993@gmail.com>
2021-09-08 17:32:56 +0600

    styling layouts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,34 @@
 
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
-    <body>
+    <body class="bg-gray-200">
+        <nav class="p-6 bg-white flex justify-between mb-6">
+            <ul class="flex item-center">
+                <li>
+                    <a href="" class="p-3">Home</a>
+                </li>
+                <li>
+                    <a href="" class="p-3">Dashboard</a>
+                </li>
+                <li>
+                    <a href="" class="p-3">Post</a>
+                </li>
+            </ul>
+            <ul class="flex item-center">
+                <li>
+                    <a href="" class="p-3">Shahriar Shaon</a>
+                </li>
+                <li>
+                    <a href="" class="p-3">Login</a>
+                </li>
+                <li>
+                    <a href="" class="p-3">Register</a>
+                </li>
+                <li>
+                    <a href="" class="p-3">Logout</a>
+                </li>
+            </ul>
+        </nav>
         @yield('content')
     </body>
 </html>
