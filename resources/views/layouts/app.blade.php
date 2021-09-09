7746b6f598f5270d040ad081991e07b6048fce04
Shaon Shahriar <shahriarshaon1993@gmail.com>
2021-09-10 01:02:00 +0600

    logging out

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
         <nav class="p-6 bg-white flex justify-between mb-6">
             <ul class="flex item-center">
                 <li>
-                    <a href="" class="p-3">Home</a>
+                    <a href="/" class="p-3">Home</a>
                 </li>
                 <li>
                     <a href="{{ route('dashboard') }}" class="p-3">Dashboard</a>
@@ -24,10 +24,13 @@
             <ul class="flex item-center">
                 @auth
                     <li>
-                        <a href="" class="p-3">Shahriar Shaon</a>
+                        <a href="" class="p-3">{{ auth()->user()->name }}</a>
                     </li>
                     <li>
-                        <a href="" class="p-3">Logout</a>
+                        <form action="{{ route('logout') }}" method="POST" class="inline p-3">
+                            @csrf
+                            <button type="submit">Logout</button>
+                        </form>
                     </li>
                 @endauth
                 @guest
