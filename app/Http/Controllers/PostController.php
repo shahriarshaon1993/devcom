42d743f52c42f56fe9632ee2011f7cf5ca04d4ef
Shaon Shahriar <shahriarshaon1993@gmail.com>
2021-09-10 03:28:11 +0600

    Pagination

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,7 +9,7 @@ class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::get();
+        $posts = Post::with('user')->paginate(2);
 
         return view('posts.index', compact('posts'));
     }
