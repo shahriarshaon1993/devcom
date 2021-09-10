30c23a33596b13d83c4f953a4a679903be0a1f90
Shaon Shahriar <shahriarshaon1993@gmail.com>
2021-09-10 07:05:33 +0600

    Authorization

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -28,14 +28,14 @@
                         <span class="text-gray-600 text-sm">{{ $post->created_at->diffForHumans() }}</span>
 
                         <p class="mb-2">{{ $post->body }}</p>
-
-                        <div>
+                        
+                        @can('delete', $post)
                             <form action="{{ route('posts.destroy', $post) }}" method="POST">
                                 @csrf
                                 @method('DELETE')
                                 <button type="submit" class="text-blue-500">Delete</button>
                             </form>
-                        </div>
+                        @endcan
 
                         <div class="flex items-center">
                             @auth
