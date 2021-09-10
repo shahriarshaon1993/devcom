03a4cccd8917b4314df7d7c73e2c5ac50da10bb6
Shaon Shahriar <shahriarshaon1993@gmail.com>
2021-09-11 02:51:06 +0600

    Blade Componentes

--- resources/views/users/posts/index.blade.php
+++ resources/views/users/posts/index.blade.php
@@ -3,49 +3,22 @@
 @section('content')
     <div class="flex justify-center">
         <div class="w-8/12 bg-white p-6 rounded-lg">
-            @if ($posts->count())
-                @foreach ($posts as $post)
-                    <div class="mb-4">
-                        <a href="{{ route('users.posts', $post->user) }}" class="font-bold">{{ $post->user->name }}</a> 
-                        <span class="text-gray-600 text-sm">{{ $post->created_at->diffForHumans() }}</span>
+            <div class="p-6">
+                <h1 class="text-2xl font-medium mb-1">{{ $user->name }}</h1>
+                <p>Posted {{ $posts->count() }} {{ Str::plural('post', $posts->count()) }}</p>
+            </div>
 
-                        <p class="mb-2">{{ $post->body }}</p>
-                        
-                        @can('delete', $post)
-                            <form action="{{ route('posts.destroy', $post) }}" method="POST">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="text-blue-500">Delete</button>
-                            </form>
-                        @endcan
+            <div class="bg-white p-6 rounded-lg">
+                @if ($posts->count())
+                    @foreach ($posts as $post)
+                        <x-post :post="$post"/>
+                    @endforeach
 
-                        <div class="flex items-center">
-                            @auth
-                                @if(!$post->likedBy(auth()->user()))
-                                    <form action="{{ route('posts.likes', $post) }}" method="POST" class="mr-1">
-                                        @csrf
-                                        <button type="submit" class="text-blue-500">Like</button>
-                                    </form>
-                                @else
-                                    <form action="{{ route('posts.likes', $post->id) }}" method="POST" class="mr-1">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="text-blue-500">Unlike</button>
-                                    </form>
-                                @endif
-                            @endauth
-
-                            <span>
-                                {{ $post->likes->count() }} {{ Str::plural('like', $post->likes->count()) }}
-                            </span>
-                        </div>
-                    </div>
-                @endforeach
-
-                {{ $posts->links() }}
-            @else
-                <p>There are no posts</p>
-            @endif
+                    {{ $posts->links() }}
+                @else
+                    <p>{{ $user->name }} does not have any posts</p>
+                @endif
+            </div>
         </div>
     </div>
 @endsection
