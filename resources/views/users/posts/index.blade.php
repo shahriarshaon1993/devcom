4e7ec396a8d67255606b4351eafc96492411a90d
Shaon Shahriar <shahriarshaon1993@gmail.com>
2021-09-10 16:37:35 +0600

    User Profile Page

--- resources/views/users/posts/index.blade.php
+++ resources/views/users/posts/index.blade.php
@@ -3,24 +3,6 @@
 @section('content')
     <div class="flex justify-center">
         <div class="w-8/12 bg-white p-6 rounded-lg">
-            <form action="{{ route('posts') }}" method="POST" class="mb-2">
-                @csrf
-                <div class="mb-4">
-                    <label for="body" class="sr-only">Body</label>
-                    <textarea name="body" id="body" cols="30" rows="4" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('body') border-red-500  @enderror"></textarea>
-
-                    @error('body')
-                        <div class="text-red-500 mt-2 text-sm">
-                            {{ $message }}
-                        </div>
-                    @enderror
-                </div>
-
-                <div>
-                    <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded font-medium">Post</button>
-                </div>
-            </form>
-
             @if ($posts->count())
                 @foreach ($posts as $post)
                     <div class="mb-4">
